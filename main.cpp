f891f457c7971257775021354126dba4423bee43
Shestakov Artem <art.shestakov@icloud.com>
2023-07-18 10:31:00 +0300

    Убрана кастомная сортировка в пользу std::sort из стандартной библиотеки + незначительные исправления.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
-#include <cstring>
-#include <stdexcept>
+#include <algorithm>
 //-----------------------------------------------------------------------------
 struct IPAddress
 {
@@ -15,25 +14,44 @@ struct IPAddress
 };
 //-----------------------------------------------------------------------------
 inline unsigned char StrToInt(const std::string& s);
-void Sort(std::vector<IPAddress>& AddressList, int octet);
 void PrintFull(std::vector<IPAddress>& AddressList); //Вывод всего списка адресов
 void Print1(std::vector<IPAddress>& AddressList); //Вывод только тех адресов, где первый октет равен 1
 void Print4670(std::vector<IPAddress>& AddressList); //Вывод только тех адресов, где первый октет равен 46, а второй 70
 void PrintAny46(std::vector<IPAddress>& AddressList); //Вывод только тех адресов, где любой октет может быть равен 46
+//-----------------------------------------------------------------------------
+bool ip_cmp(const IPAddress& lhs, const IPAddress& rhs)
+{
+    if (lhs.Address[0] != rhs.Address[0])
+    {
+        return lhs.Address[0] > rhs.Address[0];
+    }
+
+    if (lhs.Address[1] != rhs.Address[1])
+    {
+        return lhs.Address[1] > rhs.Address[1];
+    }
+
+    if (lhs.Address[2] != rhs.Address[2])
+    {
+        return lhs.Address[2] > rhs.Address[2];
+    }
+
+    return lhs.Address[3] > rhs.Address[3];
+}
 //-----------------------------------------------------------------------------
 int main()
 {
     std::vector<IPAddress> AddressList;
 
 	std::string tmp;
-	tmp.reserve(32);
+	tmp.reserve(48);
 
     size_t pos = 0;
     size_t line_num = 0;
     size_t index = 0;
 
     //Читаем построчно то, что нам передали на ввод
-	while (std::getline(std::cin, tmp/*, '\n'*/))
+	while (std::getline(std::cin, tmp))
 	{
         index = 0;
         ++line_num;
@@ -43,7 +61,7 @@ int main()
         if (pos == std::string::npos)
         {
             std::cout << "Invalid string by line " << line_num << std::endl;
-            return 1;
+            return EXIT_FAILURE;
         }
 
         tmp = tmp.substr(0, pos);
@@ -66,24 +84,21 @@ int main()
             catch (const std::exception& e)
             {
                 std::cout << "Can't convert octet to integer: " << e.what() << " by line " << line_num << std::endl;
-                return 1;
+                return EXIT_FAILURE;
             }
         }
         AddressList.emplace_back(IP);
     }
 
-    //Сортируем по всем октетам
-    Sort(AddressList, 0);
-    Sort(AddressList, 1);
-    Sort(AddressList, 2);
-    Sort(AddressList, 3);
+    std::sort(AddressList.begin(), AddressList.end(), &ip_cmp);
 
+    //Выводим адреса в соответствии с заданием
     PrintFull(AddressList);
     Print1(AddressList);
     Print4670(AddressList);
     PrintAny46(AddressList);
 
-	return 0;
+    return EXIT_SUCCESS;
 }
 //-----------------------------------------------------------------------------
 inline unsigned char StrToInt(const std::string& s)
@@ -95,40 +110,6 @@ inline unsigned char StrToInt(const std::string& s)
         return (unsigned char)i;
     }
     throw std::runtime_error(("invalid range " + s + ". Must be from 0 to 255").c_str());
-}
-//-----------------------------------------------------------------------------
-void Sort(std::vector<IPAddress>& AddressList, int octet)
-{
-    bool is_need_more = false;
-
-    for (size_t i = 0, c = AddressList.size() - 1; i < c; ++i)
-    {
-        IPAddress& ip_cur = AddressList[i];
-        IPAddress& ip_next = AddressList[i + 1];
-
-        if (octet == 0)
-        {
-            if (ip_cur.Address[octet] < ip_next.Address[octet])
-            {
-                std::swap(ip_cur, ip_next);
-                is_need_more = true;
-            }
-        }
-        else
-        {
-            if (ip_cur.Address[octet] < ip_next.Address[octet] &&
-                ip_cur.Address[octet - 1] == ip_next.Address[octet - 1])
-            {
-                std::swap(ip_cur, ip_next);
-                is_need_more = true;
-            }
-        }
-    }
-
-    if (is_need_more)
-    {
-        Sort(AddressList, octet);
-    }
 }
 //-----------------------------------------------------------------------------
 void PrintFull(std::vector<IPAddress>& AddressList)
