ee1798aacfd4dc59d35aa5768562274724c63c43
Shestakov Artem <art.shestakov@icloud.com>
2023-08-01 10:49:12 +0300

    Исправлена структура данных для IP-адреса. Убраны файлы проекта под MSVC.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,9 @@
 #include <string>
 #include <cstring> //Для сборки в github actions
 #include <algorithm>
+#include <array>
 //-----------------------------------------------------------------------------
-struct IPAddress
-{
-    IPAddress()
-    {
-        memset(Address, 0, 4);
-    };
-
-    unsigned char Address[4];
-};
+using IPAddress = std::array<unsigned char, 4>;
 //-----------------------------------------------------------------------------
 inline unsigned char StrToInt(const std::string& s);
 void PrintFull(std::vector<IPAddress>& AddressList); //Вывод всего списка адресов
@@ -22,22 +15,22 @@ void PrintAny46(std::vector<IPAddress>& AddressList); //Вывод только 
 //-----------------------------------------------------------------------------
 bool ip_cmp(const IPAddress& lhs, const IPAddress& rhs)
 {
-    if (lhs.Address[0] != rhs.Address[0])
+    if (lhs[0] != rhs[0])
     {
-        return lhs.Address[0] > rhs.Address[0];
+        return lhs[0] > rhs[0];
     }
 
-    if (lhs.Address[1] != rhs.Address[1])
+    if (lhs[1] != rhs[1])
     {
-        return lhs.Address[1] > rhs.Address[1];
+        return lhs[1] > rhs[1];
     }
 
-    if (lhs.Address[2] != rhs.Address[2])
+    if (lhs[2] != rhs[2])
     {
-        return lhs.Address[2] > rhs.Address[2];
+        return lhs[2] > rhs[2];
     }
 
-    return lhs.Address[3] > rhs.Address[3];
+    return lhs[3] > rhs[3];
 }
 //-----------------------------------------------------------------------------
 int main()
@@ -72,7 +65,7 @@ int main()
         {
             try
             {
-                IP.Address[index++] = StrToInt(tmp.substr(0, pos));
+                IP[index++] = StrToInt(tmp.substr(0, pos));
                 tmp.erase(0, pos + 1);
 
                 if (tmp.find('.') == std::string::npos && !tmp.empty())
@@ -117,7 +110,7 @@ void PrintFull(std::vector<IPAddress>& AddressList)
 {
     for (const IPAddress& ip : AddressList)
     {
-        std::cout << (int)ip.Address[0] << '.' << (int)ip.Address[1] << '.' << (int)ip.Address[2] << '.' << (int)ip.Address[3] << std::endl;
+        std::cout << (int)ip[0] << '.' << (int)ip[1] << '.' << (int)ip[2] << '.' << (int)ip[3] << std::endl;
     }
 }
 //-----------------------------------------------------------------------------
@@ -125,9 +118,9 @@ void Print1(std::vector<IPAddress>& AddressList)
 {
     for (const IPAddress& ip : AddressList)
     {
-        if (ip.Address[0] == 1)
+        if (ip[0] == 1)
         {
-            std::cout << (int)ip.Address[0] << '.' << (int)ip.Address[1] << '.' << (int)ip.Address[2] << '.' << (int)ip.Address[3] << std::endl;
+            std::cout << (int)ip[0] << '.' << (int)ip[1] << '.' << (int)ip[2] << '.' << (int)ip[3] << std::endl;
         }
     }
 }
@@ -136,9 +129,9 @@ void Print4670(std::vector<IPAddress>& AddressList)
 {
     for (const IPAddress& ip : AddressList)
     {
-        if (ip.Address[0] == 46 && ip.Address[1] == 70)
+        if (ip[0] == 46 && ip[1] == 70)
         {
-            std::cout << (int)ip.Address[0] << '.' << (int)ip.Address[1] << '.' << (int)ip.Address[2] << '.' << (int)ip.Address[3] << std::endl;
+            std::cout << (int)ip[0] << '.' << (int)ip[1] << '.' << (int)ip[2] << '.' << (int)ip[3] << std::endl;
         }
     }
 }
@@ -147,9 +140,9 @@ void PrintAny46(std::vector<IPAddress>& AddressList)
 {
     for (const IPAddress& ip : AddressList)
     {
-        if (ip.Address[0] == 46 || ip.Address[1] == 46 || ip.Address[2] == 46 || ip.Address[3] == 46)
+        if (ip[0] == 46 || ip[1] == 46 || ip[2] == 46 || ip[3] == 46)
         {
-            std::cout << (int)ip.Address[0] << '.' << (int)ip.Address[1] << '.' << (int)ip.Address[2] << '.' << (int)ip.Address[3] << std::endl;
+            std::cout << (int)ip[0] << '.' << (int)ip[1] << '.' << (int)ip[2] << '.' << (int)ip[3] << std::endl;
         }
     }
 }
